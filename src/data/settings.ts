add3ff68a28a93b998f8da6a2092cce2de05fd94
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-05 22:41:31 -0600

    initial commit

---
+++ src/data/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+  site: 'https://cannonball.littlesticks.dev',
+  name: 'Little Sticks',
+  title: 'Cannonball by Little Sticks | A splash page template',
+  description: 'This is a simple splash page template built with Astro by Little Sticks',
+}
---
+++ src/data/socials.ts
@@ -0,0 +1,27 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/jaydanurwin/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: 'https://twitter.com/littlesticksdev',
+  },
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'https://youtube.com/c/LittleSticks',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/littlesticksdev',
+  },
+  {
+    title: 'Dribbble',
+    icon: 'mdi:dribbble',
+    url: 'https://dribbble.com/littlesticksdev',  
+  }
+]
